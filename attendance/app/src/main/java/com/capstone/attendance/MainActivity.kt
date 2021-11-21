9f6cf76d2c8ea5a1caa1fcb130f866c9be4ae626
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-21 21:09:58 +0700

    add starter app

---
+++ attendance/app/src/main/java/com/capstone/attendance/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.capstone.attendance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.attendance.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+    }
+}
