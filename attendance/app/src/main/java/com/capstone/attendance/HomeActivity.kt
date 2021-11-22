ba2abf7e27097f47510287c209aee5cbd4c682cc
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-22 13:00:21 +0700

    membuat navBottom

--- attendance/app/src/main/java/com/capstone/attendance/HomeActivity.kt
+++ attendance/app/src/main/java/com/capstone/attendance/HomeActivity.kt
@@ -2,13 +2,13 @@ package com.capstone.attendance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.attendance.databinding.ActivityMainBinding
+import com.capstone.attendance.databinding.ActivityHomeBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var mainBinding: ActivityMainBinding
+class HomeActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        mainBinding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
     }
 }
