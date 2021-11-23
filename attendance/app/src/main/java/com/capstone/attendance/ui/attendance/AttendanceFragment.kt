8dc2014cf91862a9d161701838cbc3cadef61894
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-23 10:59:09 +0700

    - menghapus unused code di AttendanceFragment.kt dan AttendanceViewModel.kt - merubah tampilan dari fragment_attendance.xml - menambhkan string resources pendukung - membuat background response di drawable

--- attendance/app/src/main/java/com/capstone/attendance/ui/attendance/AttendanceFragment.kt
+++ attendance/app/src/main/java/com/capstone/attendance/ui/attendance/AttendanceFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.capstone.attendance.databinding.FragmentAttendanceBinding
 
@@ -29,11 +27,6 @@ class AttendanceFragment : Fragment() {
 
         _binding = FragmentAttendanceBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        attendanceViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 
