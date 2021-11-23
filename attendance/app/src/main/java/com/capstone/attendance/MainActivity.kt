c1fcfa770ffc4fb2eb2de77f970eeb48352efaaa
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-23 11:11:43 +0700

    hide action bar

--- attendance/app/src/main/java/com/capstone/attendance/MainActivity.kt
+++ attendance/app/src/main/java/com/capstone/attendance/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.capstone.attendance
 
 import android.os.Bundle
+import android.view.WindowManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -31,5 +32,7 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+//        hide actionBar
+        supportActionBar?.hide()
     }
 }
