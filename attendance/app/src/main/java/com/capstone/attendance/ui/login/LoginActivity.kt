20e8a1ce6b5516caffc395d7b245fa76e09dd6f5
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-23 16:28:40 +0700

    membuat listener btnsignup

--- attendance/app/src/main/java/com/capstone/attendance/ui/login/LoginActivity.kt
+++ attendance/app/src/main/java/com/capstone/attendance/ui/login/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.capstone.attendance.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.attendance.databinding.ActivityLoginBinding
+import com.capstone.attendance.ui.signup.SignupActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var loginBinding: ActivityLoginBinding
@@ -11,8 +13,13 @@ class LoginActivity : AppCompatActivity() {
         loginBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(loginBinding.root)
 //        remove actionbar
-        if (supportActionBar!=null){
+        if (supportActionBar != null) {
             supportActionBar?.hide()
+        }
+        loginBinding.btnSignUp.setOnClickListener {
+            Intent(this@LoginActivity, SignupActivity::class.java).also {
+                startActivity(it)
+            }
         }
     }
 }
