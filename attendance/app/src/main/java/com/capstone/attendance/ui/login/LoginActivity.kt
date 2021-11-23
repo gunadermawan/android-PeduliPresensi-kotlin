7cbdea45ca6f002ccf19084b8dc61c6d790a4d2e
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-23 16:23:50 +0700

    membuat ui login dan binding LoginActivity.kt

--- attendance/app/src/main/java/com/capstone/attendance/ui/login/LoginActivity.kt
+++ attendance/app/src/main/java/com/capstone/attendance/ui/login/LoginActivity.kt
@@ -1,12 +1,14 @@
 package com.capstone.attendance.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.attendance.R
+import androidx.appcompat.app.AppCompatActivity
+import com.capstone.attendance.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var loginBinding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(loginBinding.root)
     }
 }
