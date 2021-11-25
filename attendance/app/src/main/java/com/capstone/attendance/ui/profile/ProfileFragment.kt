c41221b223e65a15eef6b8e881d7aeb206a4ce2f
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-25 10:45:21 +0700

    - membuat fun untuk upload foto profile - update versi dependency - menambahkan dependency picasso untuk menampilkan gambar dari database

--- attendance/app/src/main/java/com/capstone/attendance/ui/profile/ProfileFragment.kt
+++ attendance/app/src/main/java/com/capstone/attendance/ui/profile/ProfileFragment.kt
@@ -1,7 +1,11 @@
 package com.capstone.attendance.ui.profile
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,10 +13,13 @@ import androidx.fragment.app.Fragment
 import com.capstone.attendance.databinding.FragmentProfileBinding
 import com.capstone.attendance.ui.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
 
 class ProfileFragment : Fragment() {
     private lateinit var profileBinding: FragmentProfileBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var imgUri: Uri
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,5 +40,50 @@ class ProfileFragment : Fragment() {
                 startActivity(it)
             }
         }
+        profileBinding.ivProfile.setOnClickListener {
+            intentCamera()
+        }
+    }
+
+    private fun intentCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+            activity?.packageManager?.let {
+                intent.resolveActivity(it).also {
+                    startActivityForResult(intent, REQUEST_CAMERA)
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+            val imgBitmap = data?.extras?.get("data") as Bitmap
+            uploadImgBitmap(imgBitmap)
+        }
+    }
+
+    private fun uploadImgBitmap(imgBitmap: Bitmap) {
+        val baos = ByteArrayOutputStream()
+        val ref =
+            FirebaseStorage.getInstance().reference.child("img_profile/${FirebaseAuth.getInstance().currentUser?.uid}")
+        imgBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val img = baos.toByteArray()
+//        upload
+        ref.putBytes(img)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    ref.downloadUrl.addOnCompleteListener {
+                        it.result?.let {
+                            imgUri = it
+                            profileBinding.ivProfile.setImageBitmap(imgBitmap)
+                        }
+                    }
+                }
+            }
+    }
+
+    companion object {
+        const val REQUEST_CAMERA = 100
     }
 }
