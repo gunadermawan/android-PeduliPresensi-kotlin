a67fa3b01e66919b583e7f38492de885b4a5c4ff
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-25 11:21:40 +0700

    membuat fun update profile

--- attendance/app/src/main/java/com/capstone/attendance/ui/profile/ProfileFragment.kt
+++ attendance/app/src/main/java/com/capstone/attendance/ui/profile/ProfileFragment.kt
@@ -9,11 +9,15 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.capstone.attendance.R
 import com.capstone.attendance.databinding.FragmentProfileBinding
 import com.capstone.attendance.ui.login.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
 import java.io.ByteArrayOutputStream
 
 class ProfileFragment : Fragment() {
@@ -33,6 +37,55 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        if (user != null) {
+            if (user.photoUrl != null) {
+                Picasso.get().load(user.photoUrl).into(profileBinding.ivProfile)
+            } else {
+                Picasso.get().load("https://picsum.photos/id/1/200/300")
+                    .into(profileBinding.ivProfile)
+            }
+            profileBinding.etName.setText(user.displayName)
+            profileBinding.etEmail.setText(user.email)
+            if (user.isEmailVerified) {
+                profileBinding.ivVerified.visibility = View.VISIBLE
+            } else {
+                profileBinding.ivUnverified.visibility = View.VISIBLE
+            }
+            if (user.phoneNumber.isNullOrEmpty()) {
+                profileBinding.etPhone.setText(R.string.input_phone_number)
+            } else {
+                profileBinding.etPhone.setText(user.phoneNumber)
+            }
+        }
+        profileBinding.btnUpdate.setOnClickListener {
+            val image = when {
+                ::imgUri.isInitialized -> imgUri
+                user?.photoUrl == null -> Uri.parse("https://picsum.photos/id/1/200/300")
+                else -> user.photoUrl
+            }
+            val name = profileBinding.etName.text.toString().trim()
+            if (name.isEmpty()) {
+                profileBinding.etName.error = "Nama harus diisi!"
+                profileBinding.etName.requestFocus()
+                return@setOnClickListener
+            }
+            UserProfileChangeRequest.Builder()
+                .setDisplayName(name)
+                .setPhotoUri(image)
+                .build().also {
+                    user?.updateProfile(it)?.addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Toast.makeText(
+                                activity,
+                                "Profile berhasil di update",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else
+                            Toast.makeText(activity, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
         profileBinding.btnLogout.setOnClickListener {
             auth.signOut()
             Intent(activity, LoginActivity::class.java).also {
