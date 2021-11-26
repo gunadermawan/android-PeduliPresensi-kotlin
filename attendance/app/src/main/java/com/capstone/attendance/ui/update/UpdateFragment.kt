84a13595b3e4aa6aefe107a53778616c5a9ad3d5
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-26 10:29:04 +0700

    convert java to kotlin

--- attendance/app/src/main/java/com/capstone/attendance/ui/update/UpdateFragment.kt
+++ attendance/app/src/main/java/com/capstone/attendance/ui/update/UpdateFragment.kt
@@ -1,66 +1,19 @@
-package com.capstone.attendance.ui.update;
+package com.capstone.attendance.ui.update
 
-import android.os.Bundle;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.capstone.attendance.R
 
-import androidx.fragment.app.Fragment;
+class UpdateFragment : Fragment() {
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.capstone.attendance.R;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UpdateFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class UpdateFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public UpdateFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UpdateFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UpdateFragment newInstance(String param1, String param2) {
-        UpdateFragment fragment = new UpdateFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false);
+        return inflater.inflate(R.layout.fragment_update, container, false)
     }
 }
