2451261e5cb06fa348878fe9409f95c13c6b42ae
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-26 13:45:43 +0700

    membuat fun update email pengguna

--- attendance/app/src/main/java/com/capstone/attendance/ui/update/UpdateFragment.kt
+++ attendance/app/src/main/java/com/capstone/attendance/ui/update/UpdateFragment.kt
@@ -1,19 +1,84 @@
 package com.capstone.attendance.ui.update
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.capstone.attendance.R
+import androidx.navigation.Navigation
+import com.capstone.attendance.databinding.FragmentUpdateBinding
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 
 class UpdateFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var updateBinding: FragmentUpdateBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        updateBinding = FragmentUpdateBinding.inflate(layoutInflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        return updateBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        updateBinding.layoutPassword.visibility = View.VISIBLE
+        updateBinding.layoutEmail.visibility = View.GONE
+        updateBinding.btnAuth.setOnClickListener {
+            val pass = updateBinding.etPassword.text.toString().trim()
+            if (pass.isEmpty()) {
+                updateBinding.etPassword.error = "Password harus di isi!"
+                updateBinding.etPassword.requestFocus()
+                return@setOnClickListener
+            }
+            user.let {
+                val userCredential = EmailAuthProvider.getCredential(it?.email!!, pass)
+                it.reauthenticate(userCredential).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        updateBinding.layoutPassword.visibility = View.GONE
+                        updateBinding.layoutEmail.visibility = View.VISIBLE
+                    } else if (it.exception is FirebaseAuthInvalidCredentialsException) {
+                        updateBinding.etPassword.error = "password Anda salah!"
+                        updateBinding.etPassword.requestFocus()
+                    } else {
+                        Toast.makeText(activity, "${it.exception?.message}", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+            updateBinding.btnUpdate.setOnClickListener { view ->
+                val email = updateBinding.etEmail.text.toString().trim()
+                if (email.isEmpty()) {
+                    updateBinding.etEmail.error = "Email tidak boleh kosong!"
+                    updateBinding.etEmail.requestFocus()
+                    return@setOnClickListener
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    updateBinding.etEmail.error = "Email tidak valid!"
+                    updateBinding.etEmail.requestFocus()
+                    return@setOnClickListener
+                }
+                user?.let {
+                    user.updateEmail(email).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val actionUpdatedEmail = UpdateFragmentDirections.actionUpdatedEmail()
+                            Navigation.findNavController(view).navigate(actionUpdatedEmail)
+                        } else {
+                            Toast.makeText(activity, "${it.exception?.message}", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
