7953db62ba8e6bc6c293b40c7695680e92807346
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-11-28 20:24:06 +0700

    mebuat activity_reset_password

---
+++ attendance/app/src/main/java/com/capstone/attendance/ui/resetPassword/ResetPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.capstone.attendance.ui.resetPassword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.capstone.attendance.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
