2a02344e9582cf8f430eb76149f5dc91d0d636bd
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-12-15 10:54:56 +0700

    move fun isOnline to FunctionLibrary.kt

--- attendance/app/src/main/java/com/capstone/attendance/utils/FunctionLibrary.kt
+++ attendance/app/src/main/java/com/capstone/attendance/utils/FunctionLibrary.kt
@@ -1,7 +1,11 @@
 package com.capstone.attendance.utils
 
 import android.app.Activity
+import android.content.Context
 import android.graphics.Typeface
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.util.Log
 import www.sanju.motiontoast.MotionToast
 import www.sanju.motiontoast.MotionToastStyle
 
@@ -16,5 +20,28 @@ object FunctionLibrary {
         font: Typeface?
     ) {
         MotionToast.createColorToast(context, title, message, style, position, duration, font)
+    }
+    fun checkConnection(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                    return true
+                }
+            }
+        }
+        return false
     }
 }
