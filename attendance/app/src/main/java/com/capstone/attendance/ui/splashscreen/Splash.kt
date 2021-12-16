822e838c8e99d3f8902324eba7a22494722d58f7
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-12-16 21:17:22 +0700

    - update method deprecated

--- attendance/app/src/main/java/com/capstone/attendance/ui/splashscreen/Splash.kt
+++ attendance/app/src/main/java/com/capstone/attendance/ui/splashscreen/Splash.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import com.capstone.attendance.R
 import com.capstone.attendance.ui.login.LoginActivity
 import com.capstone.attendance.utils.DELAY
@@ -14,10 +15,10 @@ class Splash : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         supportActionBar?.hide()
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }, DELAY.toLong())
+        }, DELAY)
     }
 }
