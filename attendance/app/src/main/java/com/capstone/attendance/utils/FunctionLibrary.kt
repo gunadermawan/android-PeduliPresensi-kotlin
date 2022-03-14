d860a7272e830b8832333dca5c0747e3a5ca8276
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2022-03-15 06:03:01 +0700

    update system: move syntax time Attendance to FunctionLibrary.kt

--- attendance/app/src/main/java/com/capstone/attendance/utils/FunctionLibrary.kt
+++ attendance/app/src/main/java/com/capstone/attendance/utils/FunctionLibrary.kt
@@ -10,6 +10,7 @@ import www.sanju.motiontoast.MotionToast
 import www.sanju.motiontoast.MotionToastStyle
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.*
 
 object FunctionLibrary {
     fun toast(
@@ -52,5 +53,28 @@ object FunctionLibrary {
         val currentTime = Calendar.getInstance().time
         val dateFormat = SimpleDateFormat(SIMPLE_DATE_FORMAT, Locale.getDefault())
         return dateFormat.format(currentTime)
+    }
+
+    fun calculateDistance(lat1: Double, lon1: Double, lat2: Double, long2: Double): Double {
+        val r = 6372.8
+        val radianLat1 = Math.toRadians(lat1)
+        val radianLat2 = Math.toRadians(lat2)
+        val dLat = Math.toRadians(lat2 - lat1)
+        val dLon = Math.toRadians(long2 - lon1)
+        return 2 * r * asin(
+            sqrt(
+                sin(dLat / 2).pow(2.0) + sin(dLon / 2).pow(2.0) * cos(radianLat1) * cos(
+                    radianLat2
+                )
+            )
+        )
+    }
+
+    fun timeAttendance(): Boolean {
+        return FunctionLibrary.getCurrentTime()!! > "07:00" && FunctionLibrary.getCurrentTime()!! < "09:00"
+    }
+
+    fun timeAttendanceLate(): Boolean {
+        return FunctionLibrary.getCurrentTime()!! > "09:00" && FunctionLibrary.getCurrentTime()!! < "12:00"
     }
 }
