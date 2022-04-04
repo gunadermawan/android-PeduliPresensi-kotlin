dfec61cf9897360faf701664a4434ed184639e38
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2022-04-05 06:26:18 +0700

    update System: created thread while scanning location using kotlin coroutine

--- attendance/app/src/main/java/com/capstone/attendance/viewModel/HistoryViewModel.kt
+++ attendance/app/src/main/java/com/capstone/attendance/viewModel/HistoryViewModel.kt
@@ -1,13 +1,7 @@
 package com.capstone.attendance.viewModel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HistoryViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
-    }
-    val text: LiveData<String> = _text
 }
